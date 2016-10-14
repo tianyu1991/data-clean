cd16d72d39926cdbc2663f6cd0366d653083f249
tianyu liu <hulan1991@gmail.com>
2016-10-14 17:22:54 -0400

    Update code.r

--- code.r
+++ code.r
@@ -28,3 +28,79 @@ for(i in 2:nrow(newdata)){
 		newdata[i,53]=newdata[i,53]+1
 		}
 	}
+
+
+  
+
+test<-newdata[,13:14]
+FirstAdd<-paste(newdata[,2],newdata[,13],newdata[,14],sep=" ")
+newdata<-cbind(newdata,FirstAdd)
+newdata<-newdata[order(FirstAdd),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,55]== newdata[i,55]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+LastDOB<-paste(newdata[,3],newdata[,4],sep=" ")
+newdata<-cbind(newdata,LastDOB)
+newdata<-newdata[order(LastDOB),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,56]== newdata[i,56]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+FirstAddZip<-paste(newdata[,2],newdata[,13],newdata[,15],sep=" ")
+newdata<-cbind(newdata,FirstAddZip)
+newdata<-newdata[order(FirstAddZip),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,57]== newdata[i,57]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+FirstAddZip2<-paste(newdata[,2],newdata[,14],newdata[,15],sep=" ")
+newdata<-cbind(newdata,FirstAddZip2)
+newdata<-newdata[order(FirstAddZip2),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,58]== newdata[i,58]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+DOBZip<-paste(newdata[,4],newdata[,15],sep=" ")
+newdata<-cbind(newdata,DOBZip)
+newdata<-newdata[order(DOBZip),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,59]== newdata[i,59]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+DOBAdd<-paste(newdata[,4],newdata[,13],sep=" ")
+newdata<-cbind(newdata,DOBAdd)
+newdata<-newdata[order(DOBAdd),]
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,60]== newdata[i,60]){
+		newdata[i-1,53]=newdata[i-1,53]+1
+		newdata[i,53]=newdata[i,53]+1
+		}
+	}
+
+
+write.xlsx(x=newdata, file = "dataclean.xlsx",
+        sheetName = "Test", row.names = FALSE)
+
+Sys.setenv(R_ZIPCMD= "C:/Rtools/bin/zip")
