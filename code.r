49c09243982b3147e7bf567920a8f26dab12135e
tianyu liu <hulan1991@gmail.com>
2018-01-17 16:25:35 -0500

    Update code.r

--- code.r
+++ code.r
@@ -1,5 +1,5 @@
 library("openxlsx")
-mydf <- read.xlsx("2017-01-08 All Activity Report FY17 Q2.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+mydf <- read.xlsx("Copy of Master All Activity Report 2015-2017 use this May 2017.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
 
 ##test for full name
 FullName<-paste(mydf[,2],mydf[,3],sep=" ")
@@ -9,9 +9,9 @@ mydf3<-cbind(mydf2,FullName_Count)
 
 newdata<-mydf3[order(FullName),]
 for(i in 2:nrow(newdata)){
-	if (newdata[i-1,49]== newdata[i,49]){
-		newdata[i-1,50]=newdata[i-1,50]+1
-		newdata[i,50]=newdata[i,50]+1
+	if (newdata[i-1,51]== newdata[i,51]){
+		newdata[i-1,52]=newdata[i-1,52]+1
+		newdata[i,52]=newdata[i,52]+1
 		}
 	}
 
@@ -19,24 +19,9 @@ FirstDOB<-paste(newdata[,2],newdata[,4],sep=" ")
 newdata<-cbind(newdata,FirstDOB)
 newdata<-newdata[order(FirstDOB),]
 
+
 FirstDOB_Count<-rep(0,nrow(mydf2))
 newdata<-cbind(newdata,FirstDOB_Count)
-
-for(i in 2:nrow(newdata)){
-	if (newdata[i-1,51]== newdata[i,51]){
-		newdata[i-1,52]=newdata[i-1,52]+1
-		newdata[i,52]=newdata[i,52]+1
-		}
-	}
-
-
-
-FirstAdd<-paste(newdata[,2],newdata[,12],newdata[,13],sep=" ")
-newdata<-cbind(newdata,FirstAdd)
-newdata<-newdata[order(FirstAdd),]
-
-FirstAdd_Count<-rep(0,nrow(mydf2))
-newdata<-cbind(newdata,FirstAdd_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,53]== newdata[i,53]){
@@ -46,12 +31,12 @@ for(i in 2:nrow(newdata)){
 	}
 
 
-LastDOB<-paste(newdata[,3],newdata[,4],sep=" ")
-newdata<-cbind(newdata,LastDOB)
-newdata<-newdata[order(LastDOB),]
+FirstAdd<-paste(newdata[,2],newdata[,13],newdata[,14],sep=" ")
+newdata<-cbind(newdata,FirstAdd)
+newdata<-newdata[order(FirstAdd),]
 
-LastDOB_Count<-rep(0,nrow(mydf2))
-newdata<-cbind(newdata,LastDOB_Count)
+FirstAdd_Count<-rep(0,nrow(mydf2))
+newdata<-cbind(newdata,FirstAdd_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,55]== newdata[i,55]){
@@ -60,12 +45,12 @@ for(i in 2:nrow(newdata)){
 		}
 	}
 
-FirstAddZip<-paste(newdata[,2],newdata[,12],newdata[,14],sep=" ")
-newdata<-cbind(newdata,FirstAddZip)
-newdata<-newdata[order(FirstAddZip),]
+LastDOB<-paste(newdata[,3],newdata[,4],sep=" ")
+newdata<-cbind(newdata,LastDOB)
+newdata<-newdata[order(LastDOB),]
 
-FirstAddZip_Count<-rep(0,nrow(mydf2))
-newdata<-cbind(newdata,FirstAddZip_Count)
+LastDOB_Count<-rep(0,nrow(mydf2))
+newdata<-cbind(newdata,LastDOB_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,57]== newdata[i,57]){
@@ -74,12 +59,12 @@ for(i in 2:nrow(newdata)){
 		}
 	}
 
-FirstAddZip2<-paste(newdata[,2],newdata[,13],newdata[,14],sep=" ")
-newdata<-cbind(newdata,FirstAddZip2)
-newdata<-newdata[order(FirstAddZip2),]
+FirstAddZip<-paste(newdata[,2],newdata[,13],newdata[,15],sep=" ")
+newdata<-cbind(newdata,FirstAddZip)
+newdata<-newdata[order(FirstAddZip),]
 
-FirstAddZip2_Count<-rep(0,nrow(mydf2))
-newdata<-cbind(newdata,FirstAddZip2_Count)
+FirstAddZip_Count<-rep(0,nrow(mydf2))
+newdata<-cbind(newdata,FirstAddZip_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,59]== newdata[i,59]){
@@ -88,12 +73,12 @@ for(i in 2:nrow(newdata)){
 		}
 	}
 
-DOBZip<-paste(newdata[,4],newdata[,14],sep=" ")
-newdata<-cbind(newdata,DOBZip)
-newdata<-newdata[order(DOBZip),]
+FirstAddZip2<-paste(newdata[,2],newdata[,14],newdata[,15],sep=" ")
+newdata<-cbind(newdata,FirstAddZip2)
+newdata<-newdata[order(FirstAddZip2),]
 
-DOBZip_Count<-rep(0,nrow(mydf2))
-newdata<-cbind(newdata,DOBZip_Count)
+FirstAddZip2_Count<-rep(0,nrow(mydf2))
+newdata<-cbind(newdata,FirstAddZip2_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,61]== newdata[i,61]){
@@ -102,7 +87,21 @@ for(i in 2:nrow(newdata)){
 		}
 	}
 
-DOBAdd<-paste(newdata[,4],newdata[,12],sep=" ")
+DOBZip<-paste(newdata[,4],newdata[,15],sep=" ")
+newdata<-cbind(newdata,DOBZip)
+newdata<-newdata[order(DOBZip),]
+
+DOBZip_Count<-rep(0,nrow(mydf2))
+newdata<-cbind(newdata,DOBZip_Count)
+
+for(i in 2:nrow(newdata)){
+	if (newdata[i-1,63]== newdata[i,63]){
+		newdata[i-1,64]=newdata[i-1,64]+1
+		newdata[i,64]=newdata[i,64]+1
+		}
+	}
+
+DOBAdd<-paste(newdata[,4],newdata[,13],sep=" ")
 newdata<-cbind(newdata,DOBAdd)
 newdata<-newdata[order(DOBAdd),]
 
@@ -111,9 +110,9 @@ DOBAdd_Count<-rep(0,nrow(mydf2))
 newdata<-cbind(newdata,DOBAdd_Count)
 
 for(i in 2:nrow(newdata)){
-	if (newdata[i-1,63]== newdata[i,63]){
-		newdata[i-1,64]=newdata[i-1,64]+1
-		newdata[i,64]=newdata[i,64]+1
+	if (newdata[i-1,65]== newdata[i,65]){
+		newdata[i-1,66]=newdata[i-1,66]+1
+		newdata[i,66]=newdata[i,66]+1
 		}
 	}
 
@@ -123,12 +122,12 @@ newdata<-cbind(newdata,EnterID_Count)
 
 for(i in 2:nrow(newdata)){
 	if (newdata[i-1,1]== newdata[i,1]){
-		newdata[i-1,65]=newdata[i-1,65]+1
-		newdata[i,65]=newdata[i,65]+1
+		newdata[i-1,67]=newdata[i-1,67]+1
+		newdata[i,67]=newdata[i,67]+1
 		}
 	}
 
-Total_Count<-newdata[,65]+newdata[,64]+newdata[,62]+newdata[,60]+newdata[,58]+newdata[,56]+newdata[,54]+newdata[,52]+newdata[,50]
+Total_Count<-newdata[,67]+newdata[,66]+newdata[,64]+newdata[,62]+newdata[,60]+newdata[,58]+newdata[,56]+newdata[,54]+newdata[,52]
 newdata2<-cbind(newdata,Total_Count)
 
 Sys.setenv(R_ZIPCMD= "C:/Rtools/bin/zip")
